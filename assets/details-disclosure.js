a539ab7eac0d40ee764eaf7f1ff5ee4ad3fcbdc3
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2024-10-22 16:41:41 -0400

    _+ Refactoring details drawer code completely for faqslist page and faqs-preview
     + Adjusting css andhtml for faq page and finishing it up

--- assets/details-disclosure.js
+++ assets/details-disclosure.js
@@ -48,3 +48,77 @@ class HeaderMenu extends DetailsDisclosure {
 }
 
 customElements.define('header-menu', HeaderMenu);
+
+
+
+if (!customElements.get('drawer-disclosure')) {
+  class DrawerDisclosure extends HTMLElement {
+    constructor() {
+      super();
+      this.mainDetailsToggle = this.querySelector('details');
+      this.mainDetailsToggle.addEventListener('click', this.toggleDrawer.bind(this));
+      this.querySelector('[data-drawer-content]').style.height = 0;
+      this.mainDetailsToggle.addEventListener('focusout', this.onFocusOut.bind(this));
+      this.mainDetailsToggle.addEventListener('keydown', this.onKeyDown.bind(this));
+      this.animationLength = 450; 
+    }
+
+    connectedCallback() {
+      if (Shopify.designMode) {
+        document.addEventListener('shopify:block:select', this.oncontentBlockSelect.bind(this));
+      }
+    }
+
+    onKeyDown(event) {
+      if (event.key === 'Escape') {
+        this.closeDrawer(this);
+      }
+    }
+
+    onFocusOut() {
+      setTimeout(() => {
+        if (!this.contains(document.activeElement)) this.closeDrawer(this);
+      });
+    }
+
+    toggleDrawer(event) {
+      event.preventDefault();
+
+      if(this.mainDetailsToggle.getAttribute('open')) {
+        this.closeDrawer(this);
+      } else {
+        this.openDrawer(event.currentTarget);
+      }
+    }
+
+    closeDrawer(pElem) {
+      if (!pElem) return;
+
+      pElem.querySelector('[data-drawer-content]').style.height = 0;
+      pElem.querySelector('summary').setAttribute('aria-expanded', false);
+
+      setTimeout(() => {
+        pElem.querySelector('details').removeAttribute('open');
+      }, this.animationLength);
+    }
+
+    openDrawer(pDrawer) {
+      const parentDetails = pDrawer.closest('details');
+
+      if (parentDetails && !parentDetails.getAttribute('open')) {
+        parentDetails.setAttribute('open', true);
+        const contentAnswer = parentDetails.querySelector('[data-drawer-content]');
+        contentAnswer.style.height = `${contentAnswer.querySelector('[data-drawer-content-inner]').offsetHeight}px`;
+        pDrawer.setAttribute('aria-expanded', true);
+        this.activeDrawer = parentDetails.dataset.id;
+      }
+    }
+
+    oncontentBlockSelect(event) {
+      event.preventDefault();
+      setTimeout(() => this.openDrawer(event.target), 200);
+    }
+  }
+
+  customElements.define('drawer-disclosure', DrawerDisclosure);
+}
